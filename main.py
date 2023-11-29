2e5f3c37775203df4e511a1609bc243351232985
PC <lozovkyyd@gmail.com>
2023-11-29 17:19:51 +0200

    Commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import time
+from time import sleep
+print('Добрий день.')
+time.sleep(2)
+print('Поставте будь ласка хорошу оцінку.')
+time.sleep(2)
+print('Радий був з вами поговорити.')
+time.sleep(2)
+print('Допобачення.')
+time.sleep(5)
